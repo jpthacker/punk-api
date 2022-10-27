4b2844e0794bae697376d0ddcedb8026e2fefc00
Jack Thacker <jackthacker89@gmail.com>
2022-10-27 16:02:06 +0100

    Added styles to BeerInfo

--- src/components/BeerInfo/BeerInfo.jsx
+++ src/components/BeerInfo/BeerInfo.jsx
@@ -7,18 +7,29 @@ const BeerInfo = (props) => {
   return (
     <section className={styles.beerInfo}>
       <div className={styles.beerBanner}>
+        <h4 className={styles.header}>{name}</h4>
         <div className={styles.imgContainer}>
           <img src={image_url} alt="beer image" />
         </div>
       </div>
       <div className={styles.beerText}>
-        <h2 className={styles.header}>{name}</h2>
-        <h4 className={styles.tagline}>{tagline}</h4>
+        <h2 className={styles.tagline}>{tagline}</h2>
         <p className={styles.description}>{description}</p>
         <div className={styles.beerFigures}>
-          <div className={styles.figure}>{abv}%</div>
-          <div className={styles.figure}>{ibu}</div>
-          <div className={styles.figure}>{first_brewed}</div>
+          <div className={styles.figureContainer}>
+            <p>ABV</p>
+            <h2 className={styles.figure}>{abv}%</h2>
+          </div>
+          <span></span>
+          <div className={styles.figureContainer}>
+            <p>IBU</p>
+            <h2 className={styles.figure}>{ibu}</h2>
+          </div>
+          <span></span>
+          <div className={styles.figureContainer}>
+            <p>First Brewed</p>
+            <h2 className={styles.figure}>{first_brewed}</h2>
+          </div>
         </div>
       </div>
     </section>
